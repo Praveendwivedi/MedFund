fe0f2d9cb512e57bbf65c88ac0bef8ed3cbf6a92
jyoti-2 <jyoti02.nitt@gmail.com>
2022-06-12 11:36:22 +0530

    Initial SetUp

---
+++ src/medfund_assets/src/Components/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Login.css";
+
+class Login extends React.Componenet{
+    render(){
+        return(
+            <div className="login-component">
+
+            </div>
+        )
+    }
+}
+export default Login;
